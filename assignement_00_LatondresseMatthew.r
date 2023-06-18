69bca6501f2534378d1f04f8757b040fb1c14270
Matt15137 <119823479+Matt15137@users.noreply.github.com>
2023-06-18 15:16:19 -0400

    Add files via upload

---
+++ assignement_00_LatondresseMatthew.r
@@ -0,0 +1,57 @@
+# Assignment: ASSIGNMENT 0
+# Name: Latondresse, Matthew
+# Date: 6/18/2023
+
+# Basics
+
+## Add 8 and 5
+8+5#=>13
+
+
+
+## Subtract 6 from 22
+22-6#=>16
+
+## Multiply 6 by 7
+7*6#=>42
+
+## Add 4 to 6 and divide the result by 2
+(4+6)/2#=>5
+
+## Compute 5 modulo 2
+5%%2#=>1
+
+## Assign the value 82 to the variable x
+## Print x
+x<-82
+print(x)
+
+## Assign the value 41 to the variable y
+## Print y
+y<- 41
+print(y)
+
+## Assign the output of x + y to the variable z
+## Print z
+z<-x + y
+print(z)
+
+## Assign the string value "DSC520" to the variable class_name
+class_name<-"DSC520"
+## Print the value of class_name
+print(class_name)
+
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good<- TRUE
+print(is_good)
+
+## Check the class of the variable is_good using the `class()` function
+print(class(is_good))
+
+## Check the class of the variable z using the `class()` function
+print(class(z))
+
+## Check the class of the variable class_name using the class() function
+print(class(class_name))
+
